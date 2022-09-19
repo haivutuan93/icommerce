2f96b5bf86047878e02f9e523586bbc5387a0d8b
Vu Tuan Hai <haivutuan93@gmail.com>
2022-09-19 17:08:33 +0700

    Update ProductService.java

--- src/main/java/com/nab/icommerce/service/ProductService.java
+++ src/main/java/com/nab/icommerce/service/ProductService.java
@@ -60,7 +60,7 @@ public class ProductService {
     }
 
     public List<ProductInformation> filterProduct(ProductFilterRequest request){
-        log.info("Filter Product with name: {}, brand: {}, colour:{}, category: {}",
+        log.info("Filter Production with name: {}, brand: {}, colour:{}, category: {}",
                 request.getName(), request.getBrand(), request.getColour(), request.getCategory());
 
         Query query = new Query();
