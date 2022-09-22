0e5da405645454c4da0bbeb0e6f3c2b1bb8afa31
Vu Tuan Hai <haivutuan93@gmail.com>
2022-09-22 07:46:13 +0700

    add check jenkins

--- src/main/java/com/nab/icommerce/service/ProductService.java
+++ src/main/java/com/nab/icommerce/service/ProductService.java
@@ -62,7 +62,7 @@ public class ProductService {
     }
 
     public List<ProductInformation> filterProduct(ProductFilterRequest request){
-        log.info("Filter Production with name: {}, brand: {}, colour:{}, category: {}",
+        log.info("Filter Product with name: {}, brand: {}, colour:{}, category: {}",
                 request.getName(), request.getBrand(), request.getColour(), request.getCategory());
 
         Query query = new Query();
